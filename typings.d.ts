afbbc8255e03e59cdb9eb9e0ecfb87942d2b4432
Stephen Francis <stephen.d.francis@gmail.com>
2018-04-25 10:52:31 +0100

    include types to suppress errors

---
+++ typings.d.ts
@@ -0,0 +1,48 @@
+
+declare module "rehype-document" {
+  var _rehype_document: any;
+  export = _rehype_document;
+}
+
+
+declare module "rehype-format" {
+  var _rehype_format: any;
+  export = _rehype_format;
+}
+
+
+declare module "rehype-stringify" {
+  var _rehype_stringify: any;
+  export = _rehype_stringify;
+}
+
+
+declare module "remark-mermaid" {
+  var _remark_mermaid: any;
+  export = _remark_mermaid;
+}
+
+
+declare module "remark-parse" {
+  var _remark_parse: any;
+  export = _remark_parse;
+}
+
+
+declare module "remark-rehype" {
+  var _remark_rehype: any;
+  export = _remark_rehype;
+}
+
+
+declare module "unified" {
+  var _unified: any;
+  export = _unified;
+}
+
+
+declare module "vfile-reporter" {
+  var _vfile_reporter: any;
+  export = _vfile_reporter;
+}
+
